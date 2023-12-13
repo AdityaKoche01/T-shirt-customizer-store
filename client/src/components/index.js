cf981ca7572e5a81e7bef2512f805c0cf1257408
AdityaKoche01 <adityakoche777@gmail.com>
2023-12-13 21:43:27 +0530

    first commit

---
+++ client/src/components/index.js
@@ -0,0 +1,14 @@
+import CustomButton from "./CustomButton";
+import AIpicker from "./AIPicker";
+import ColorPicker from "./ColorPicker";
+import FilePicker from "./FilePicker";
+import Tab from "./Tab";
+
+export{
+    CustomButton,
+    AIpicker,
+    ColorPicker,
+    FilePicker,
+    Tab,
+
+};
